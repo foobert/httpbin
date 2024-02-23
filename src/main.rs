286ff081c98f986d7120d823f76cf48346aa5b92
foobert <0x002a@gmail.com>
2024-02-23 18:49:32 +0100

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use rocket::http::ContentType;
-use rocket_db_pools::{Database, Connection};
 use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::{Connection, Database};
 
 #[derive(Database)]
 #[database("sqlite_data")]
@@ -17,18 +18,25 @@ fn index() -> &'static str {
 
 #[get("/data/<id>")]
 async fn read(mut db: Connection<Data>, id: &str) -> Option<(ContentType, String)> {
-   sqlx::query("SELECT payload, type FROM data WHERE id = ?").bind(id)
-       .fetch_one(&mut **db).await
-       .and_then(|row| {
-           let payload: String = row.try_get(0)?;
-           let content_type: String = row.try_get(1)?;
-           Ok((ContentType::parse_flexible(&content_type).unwrap(), payload))
-       })
-       .ok()
+    sqlx::query("SELECT payload, type FROM data WHERE id = ?")
+        .bind(id)
+        .fetch_one(&mut **db)
+        .await
+        .and_then(|row| {
+            let payload: String = row.try_get(0)?;
+            let content_type: String = row.try_get(1)?;
+            Ok((ContentType::parse_flexible(&content_type).unwrap(), payload))
+        })
+        .ok()
 }
 
 #[put("/data/<id>", data = "<payload>")]
-async fn write(mut db: Connection<Data>, id: &str, payload: &str, content_type: &ContentType) -> Option<&'static str> {
+async fn write(
+    mut db: Connection<Data>,
+    id: &str,
+    payload: &str,
+    content_type: &ContentType,
+) -> Option<&'static str> {
     sqlx::query("INSERT INTO data (id, type, timestamp, payload) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET type = $2, timestamp = $3, payload = $4")
         .bind(id)
         .bind(content_type.to_string())
@@ -41,5 +49,7 @@ async fn write(mut db: Connection<Data>, id: &str, payload: &str, content_type: 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().attach(Data::init()).mount("/", routes![index, read, write])
+    rocket::build()
+        .attach(Data::init())
+        .mount("/", routes![index, read, write])
 }
